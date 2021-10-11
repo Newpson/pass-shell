af513e581f092dcb1783a504a04e3ae25c73136d
Newpson <dk.newpson@gmail.com>
2021-10-11 23:00:09 +0300

    Refactoring
    Changed arguments handling method, so there is no need to add 'p' key anymore to get password.
    Added "remove" feature, just add 'r' key to remove specified account.

--- src/ppass.sh
+++ src/ppass.sh
@@ -1,37 +1,59 @@
-store="$HOME/.password-store"
+#	ppass.sh - tiny shell for 'pass'
+#	Copyright (C) 2021  Newpson
+#	
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 notify ()
 {
-	notify-send -i password "Pass" "$1"
+	notify-send -i password "Pass Shell" "$1"
 }
 
-if [ $# -lt 2 ]
+if [ $# -lt 1 ] || [ $# -gt 2 ]
 then
-	notify "Use: ppass <account> <u/p> OR ppass add <account>"
+	notify "Syntax error. See <i>ppass(1)</i>"
 	exit 1
 fi
 
-if ! ([ "$2" = "p" ] && [ "$2" = "u" ]) && [ "$1" = "add" ]
+NSOD=0
+store="$HOME/.password-store"
+if ! filename=$(ls $store/$1); then NSOD=1; fi
+primary=${filename%.*}
+
+if [ $# -eq 1 ]
 then
-	pass generate -nc $2 16
-	notify "$2: generated and copied for 45 s"
+	if  [ $NSOD -eq 1 ]; then notify "$1: No such account"; exit 1; fi
+	pass -c $1/$primary
+	notify "$1: password copied for 45 s"
 	exit 0
-else
-	if [ -e "$store/$1" ]
-	then
-		filename=$(ls $store/$1)
-		username=${filename%.*}
-	else
-		notify "No such account name"
-		exit 2
-	fi
 fi
 
-if [ "$2" = "u" ]
-then
-	echo $username | xclip -selection clipboard
-	notify "$1: username copied for 45 s"
-else
-	pass -c $1/$username
-	notify "$1: password copied for 45 s"
-fi
+case $2 in
+	u)
+		if [ $NSOD -eq 1 ]; then notify "$1: No such account"; exit 1; fi
+		echo $primary | xclip -selection clipboard
+		notify "$1: primary info copied for 45 s"
+		;;
+	a)
+		pass generate -nc $1 16
+		notify "$1: password generated and copied for 45 s"
+		exit 0
+		;;
+	r)
+		pass delete -rf $1
+		;;
+	*)
+		notify "No such key. See ppass(1)"
+		exit 3
+		;;
+esac
